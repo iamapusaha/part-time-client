6bc381516ee064d6033652499e64a0fa3aaf474f
iamapusaha <apusaha392@gmail.com>
2023-11-06 23:33:56 +0600

    update job details page

--- src/pages/JobDetails/JobDetails.jsx
+++ src/pages/JobDetails/JobDetails.jsx
@@ -10,10 +10,10 @@ import axios from 'axios';
 import { useLoaderData } from "react-router-dom";
 
 const JobDetails = () => {
-    const { title, email, date, photo } = useLoaderData();
+    const { title, photo, discription, date, minPrice, maxPrice, email } = useLoaderData();
     const MySwal = withReactContent(Swal)
     const { user } = useContext(AuthContext)
-
+    console.log(user.email, email);
 
     const handleBidJob = e => {
         e.preventDefault()
@@ -47,12 +47,29 @@ const JobDetails = () => {
 
     return (
         <div>
-            <div>
-                <h1>{title}</h1>
+            <div className="flex justify-center items-center border rounded-md">
+                <div className="">
+                    <div className="flex justify-center">
+                        <img
+                            src={photo}
+                            className="rounded-t-md"
+                        />
+                    </div>
+                    <div className="text-center">
+                        <h1 className="font-bold text-xl p-2">{title}</h1>
+                        <h1 className="font-semibold"><h2>Deadline: {date}</h2></h1>
+                        <h1 className="font-semibold">Price range: {minPrice} to {maxPrice}</h1>
+                        <div className="flex justify-center">
+                            <p className="font-semibold px-3">
+                                {discription}
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>
                 <div className="container mx-auto my-6 bg-[#F6F6F6] p-5 rounded">
-                    <h1 className="text-5xl text-center my-2">Add New Product</h1>
+                    <h1 className="text-5xl text-center my-2">Bid Now</h1>
                     <form onSubmit={handleBidJob}>
                         <div className="md:flex gap-3 px-2 md:px-1 mb-6">
                             <div className="form-control w-full">
@@ -92,7 +109,11 @@ const JobDetails = () => {
                             </div>
 
                         </div>
-                        <input className="btn btn-block bg-[#4D55D0] text-white" type="submit" value="Bid on the project" />
+                        {
+                            email === user.email ?
+                                <input disabled="disabled" className=" btn btn-block bg-[#121216] text-white" type="submit" value="Bid on the project" />
+                                : <input className="btn btn-block bg-[#4D55D0] text-white" type="submit" value="Bid on the project" />
+                        }
                     </form>
                 </div>
             </div>
